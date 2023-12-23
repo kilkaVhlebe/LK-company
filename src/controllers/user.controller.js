0ae5ba4c74002eccc8f50f98da9514414fa84e99
tugaserya <artempoznakov990@gmail.com>
2023-12-23 13:52:24 +0300

    debuged and fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const db = require('../DatabaseConfiguration/db')
-const UserChecker = require('../User.Checker')
+const {UserChecker} = require('../User.Checker')
 class UserController {
     async login(req, res) {
         try{
@@ -7,7 +7,7 @@ class UserController {
             const UserCheck = await db.query(`SELECT * FROM users WHERE login = $1 AND password = $2;`,
                 [login,password])
             if(UserCheck.rows.length === 1) {
-                res.status(200).json(UserCheck)
+                res.status(200).json(UserCheck.rows)
             } else {
                 res.status(404).json({message: "User not found!"})
             }
