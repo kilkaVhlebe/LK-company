5198f3246819c55b3aef0a51890bd2e423859a4b
tugaserya <artempoznakov990@gmail.com>
2023-12-23 13:12:22 +0300

    login and logout is complete(not debuged)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,34 @@
 const db = require('../DatabaseConfiguration/db')
-const bcrypt = require('bcrypt')
-
+const UserChecker = require('../User.Checker')
 class UserController {
     async login(req, res) {
-
+        try{
+            const {login, password} = req.body
+            const UserCheck = await db.query(`SELECT * FROM users WHERE login = $1 AND password = $2;`,
+                [login,password])
+            if(UserCheck.rows.length === 1) {
+                res.status(200).json(UserCheck)
+            } else {
+                res.status(404).json({message: "User not found!"})
+            }
+        }catch (error){
+            res.status(500).json({message:"Internal server error!"})
+            console.log("login error: " + error)
+        }
     }
 
     async logout(req, res) {
-
+        try{
+            const {id, login, password} = req.body
+            if (!await UserChecker(id, login, password)) {
+                res.status(404).json({message: "User not found!"})
+                return;
+            }
+            res.status(200).json({message: "User is successful"})
+        }catch (error){
+            res.status(500).json({message:"Internal server error!"})
+            console.log("logout error: " + error)
+        }
     }
 }
 
