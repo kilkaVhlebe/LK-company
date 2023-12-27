1d2298a3d1e660ffcdd85c371be474f5c5127640
tugaserya <artempoznakov990@gmail.com>
2023-12-27 13:35:13 +0300

    add new tables

--- src/DatabaseConfiguration/database.sql
+++ src/DatabaseConfiguration/database.sql
@@ -2,26 +2,89 @@ create database lk_company;
 
 CREATE TABLE users
 (
-    id        SERIAL PRIMARY KEY,
-    login     VARCHAR(50)  NOT NULL,
-    password  VARCHAR(100) NOT NULL,
-    avatar TEXT
+    id          SERIAL PRIMARY KEY,
+    login       VARCHAR(50)  NOT NULL,
+    password    VARCHAR(100) NOT NULL,
+    avatar      TEXT
 );
 
 INSERT INTO users (login, password, avatar) VALUES ('first_user', 1234, '0'); /* Написать скрипт для создания пользователей*/
-
+/*Мои договоры*/
 CREATE TABLE договоры
 (
     id SERIAL PRIMARY KEY,
-    номер   INTEGER,
-    тема    VARCHAR(50),
-    дата_начала DATE,
-    дата_окончания DATE,
-    расчетчик_ФИО TEXT,
+    номер               INTEGER,
+    тема                VARCHAR(50),
+    дата_начала         DATE,
+    дата_окончания      DATE,
+    расчетчик_ФИО       TEXT,
     расчетчик_телефона  INTEGER,
-    договорник_ФИО_1    TEXT,
-    договорник_ФИО_2    TEXT
+    договорник_ФИО      TEXT,
+    договорник_телефон  INTEGER
 );
 
 INSERT INTO договоры (номер,тема,дата_начала,дата_окончания,расчетчик_ФИО,расчетчик_телефона,договорник_ФИО_1,договорник_ФИО_2)
 VALUES (34643346, 'это тема договора', '2023-12-24','2024-3-24','Иван Иванов Иванович',985,'Иван Иванов Иванович','Иван Иванов Иванович');
+
+
+/*Карточка организации*/
+CREATE TABLE общая_информация
+(
+id SERIAL PRIMARY KEY,
+краткое название    TEXT,
+полное название     TEXT,
+юридичемкий_адрес   VARCHAR(256),
+фактический_адрес   VARCHAR(256)
+);
+
+CREATE TABLE реквизиты_компании
+(
+id SERIAL PRIMARY KEY,
+инн                 INTEGER,
+кпп                 INTEGER,
+огрн                INTEGER,
+окпо                INTEGER,
+оконх               INTEGER,
+оквэд               INTEGER,
+ндс                 INTEGER,
+вид_деятельности    VARCHAR(256),
+id_организации      INTEGER NOT NULL,
+FOREIGN (id_организации) REFERENCES общая_информация (id)
+);
+
+CREATE TABLE расчетный_счет
+(
+id SERIAL PRIMARY KEY,
+расчетный_счет      INTEGER,
+бик                 INTEGER,
+Банк                VARCHAR(255),
+id_организации      INTEGER NOT NULL,
+FOREIGN (id_организации) REFERENCES общая_информация (id)
+);
+
+CREATE TABLE контакты
+(
+id SERIAL PRIMARY KEY,
+режим_работы        VARCHAR(5),
+телефон             INTEGER,
+факс                VARCHAR(50),
+email               VARCHAR(256),
+сайт                VARCHAR(2048),
+id_организации      INTEGER NOT NULL,
+FOREIGN (id_организации) REFERENCES общая_информация (id)
+);
+
+CREATE TABLE представитель_организации
+(
+id                  SERIAL PRIMARY KEY,
+ФИО                 TEXT,
+должность           VARCHAR(256),
+юридичемкий_адрес   VARCHAR(256),
+фактический_адрес   VARCHAR(256),
+телефон             INTEGER,
+email               VARCHAR(256),
+примечание          TEXT,
+доп_информация      TEXT,
+id_организации      INTEGER NOT NULL,
+FOREIGN (id_организации) REFERENCES общая_информация (id)
+);
