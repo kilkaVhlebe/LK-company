3d4af2dc384fcde04898945070fb6174a5b3be9a
tugaserya <artempoznakov990@gmail.com>
2023-12-29 20:19:24 +0300

    new routes connect

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const app = express()
 app.use(express.json());
 
 const userRouter = require('./routes/user.routes');
-app.use("/", userRouter)
-
+app.use('/', userRouter)
+const conractRouter = require('./routes/договоры.routes')
+app.use('/', conractRouter)
 app.listen(Port, () =>{
     console.log("Server started on address http://localhost:4200")
 })
