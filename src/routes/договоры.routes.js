420d00b7e4dcfad254c623d9ab1a7119cf1e3a9c
tugaserya <artempoznakov990@gmail.com>
2024-01-12 15:18:59 +0300

    new rout

--- src/routes/договоры.routes.js
+++ src/routes/договоры.routes.js
@@ -4,5 +4,6 @@ const ContractController = require('../controllers/договоры.controller')
 
 router.post('/get/contract', ContractController.GetContract)
 router.post('/get/contract/card', ContractController.GetContractCard)
+router.post('/get/organisation/card', ContractController.GetOrganisationCard)
 
 module.exports = router
