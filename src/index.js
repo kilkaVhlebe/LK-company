6e62182c28cc643bff2e16958b16ac09f4b3c1d6
tugaserya <artempoznakov990@gmail.com>
2024-02-21 18:22:21 +0300

    new module

--- src/index.js
+++ src/index.js
@@ -9,8 +9,10 @@ app.use(express.json());
 
 const userRouter = require('./routes/user.routes');
 app.use('/', userRouter)
-const conractRouter = require('./routes/договоры.routes')
-app.use('/', conractRouter)
+const contractRouter = require('./routes/договоры.routes')
+app.use('/', contractRouter)
+const meteringPointsRouter = require('./routes/точки_учета.routes')
+app.use('/', meteringPointsRouter)
 app.listen(Port, () =>{
     console.log("Server started on address http://localhost:4200")
 })
