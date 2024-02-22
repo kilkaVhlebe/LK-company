2cb7e4a5ec68497dfab0ff111e4f5c356f3d1c97
tugaserya <artempoznakov990@gmail.com>
2024-02-22 09:46:32 +0300

    refactor

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,13 @@
 const db = require('../DatabaseConfiguration/db')
-const {UserChecker} = require('../User.Checker')
+
 class UserController {
     async login(req, res) {
         try{
             const {login, password} = req.body
-            const UserCheck = await db.query(`SELECT * FROM users WHERE login = $1 AND password = $2;`,
-                [login,password])
-            if(UserCheck.rows.length === 1) {
-                res.status(200).json(UserCheck.rows)
-            } else {
+            const UserCheck = await db.query(`SELECT * FROM users WHERE login = $1 AND password = $2;`, [login,password])
+            UserCheck.rows.length === 1 ?
+                res.status(200).json(UserCheck.rows) :
                 res.status(404).json({message: "User not found or invalid arguments!"})
-            }
         }catch (error){
             res.status(500).json({message:"Internal server error!"})
             console.error("login error: " + error)
--- src/controllers/договоры.controller.js
+++ src/controllers/договоры.controller.js
@@ -6,17 +6,15 @@ class ContractsController {
 
     async GetContract(req, res){
         try{
-            const{id, login, password} = req.body
+            const{id, login, password} = req.params
             if (!await UserChecker(id, login, password)) {
                 res.status(404).json({message: "User not found or invalid arguments!"})
                 return;
             }
             const AllUserContracts = await db.query(`SELECT * FROM договоры WHERE users_id = $1;`,[id])
-            if(AllUserContracts.rows.length !== 0){
-                res.status(200).json(AllUserContracts.rows)
-            } else{
+            AllUserContracts.rows.length !== 0 ?
+                res.status(200).json(AllUserContracts.rows) :
                 res.status(404).json({message: "You have no contracts!"})
-            }
         }catch (error){
             res.status(500).json({message:"Internal server error!"})
             console.error("Get contract error: " + error)
@@ -25,17 +23,15 @@ class ContractsController {
 
     async GetContractCard(req, res){
         try{
-            const{id, login, password} = req.body
+            const{id, login, password} = req.params
             if (!await UserChecker(id, login, password)) {
                 res.status(404).json({message: "User not found or invalid arguments!"})
                 return;
             }
             const AllUserContractsCards = await db.query(`SELECT * FROM карточка_договора WHERE user_id = $1;`,[id])
-            if(AllUserContractsCards.rows.length !== 0){
-                res.status(200).json(AllUserContractsCards.rows)
-            } else{
+            AllUserContractsCards.rows.length !== 0 ?
+                res.status(200).json(AllUserContractsCards.rows) :
                 res.status(404).json({message: "You have no contracts!"})
-            }
         }catch (error){
             res.status(500).json({message:"Internal server error!"})
             console.error("Get contract error: " + error)
@@ -44,7 +40,7 @@ class ContractsController {
 
     async GetOrganisationCard(req,res){
         try{
-            const{id, login, password} = req.body
+            const{id, login, password} = req.params
             if (!await UserChecker(id, login, password)) {
                 res.status(404).json({message: "User not found or invalid arguments!"})
                 return;
@@ -57,11 +53,9 @@ class ContractsController {
                 LEFT JOIN представитель_организации ON общая_информация.id = представитель_организации.id
                 WHERE общая_информация.id = $1;
                 `, [id])
-            if(OrganisationInfo.rows.length !== 0){
-                res.status(200).json(OrganisationInfo.rows)
-            } else{
+            OrganisationInfo.rows.length !== 0 ?
+                res.status(200).json(OrganisationInfo.rows) :
                 res.status(404).json({message: "Not Found!"})
-            }
         } catch (error){
             res.status(500).json({message:"Internal server error!"})
             console.error("Get Organisation card error: " + error)
