e6f0e35cc746c5fb3571aeec786cb10a7df92195
tugaserya <artempoznakov990@gmail.com>
2024-02-22 09:46:46 +0300

    refactor

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,5 @@ const router = new Router()
 const UserController = require('../controllers/user.controller')
 
 router.post('/login', UserController.login)
-router.post('/logout', UserController.logout)
 
 module.exports = router
--- src/routes/договоры.routes.js
+++ src/routes/договоры.routes.js
@@ -2,8 +2,8 @@ const  Router = require('express')
 const router = new Router()
 const ContractController = require('../controllers/договоры.controller')
 
-router.post('/get/contract', ContractController.GetContract)
-router.post('/get/contract/card', ContractController.GetContractCard)
-router.post('/get/organisation/card', ContractController.GetOrganisationCard)
+router.get('/get/contract', ContractController.GetContract)
+router.get('/get/contract/card', ContractController.GetContractCard)
+router.get('/get/organisation/card', ContractController.GetOrganisationCard)
 
 module.exports = router
