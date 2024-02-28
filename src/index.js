41d38e007b3804e405c634f7be72f594f40e50b4
tugaserya <artempoznakov990@gmail.com>
2024-02-28 20:14:58 +0300

    add CORS

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@ const app = express()
 
 const swaggerDocument = YAML.load(path.join(__dirname, '../docs', 'docs.yaml'));
 
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
 
+app.use(allowCrossDomain)
 app.use(express.json());
 
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
